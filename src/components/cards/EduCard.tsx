bfb81ef6ef0791fb2e487f3dcc4be644a14f9502
jettblu <jettblu.fabrications@gmail.com>
2023-04-26 00:18:45 -0400

    Update edu card style

--- src/components/cards/EduCard.tsx
+++ src/components/cards/EduCard.tsx
@@ -18,7 +18,7 @@ type Props = {
 const EduCard: NextPage<Props> = (props) => {
   const { title, description, img } = { ...props };
   return (
-    <div className="w-full bg-purple-900/20 rounded-xl border border-purple-900 px-2 py-4">
+    <div className="w-full bg-gray-400/40 dark:bg-purple-900/20 rounded-xl border border-purple-900 px-2 py-4">
       <div className="flex flex-row space-x-4">
         <div className="flex flex-col space-y-2 text-left">
           <div className="text-3xl font-bold">{title}</div>
